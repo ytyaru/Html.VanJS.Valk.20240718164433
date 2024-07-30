496a66f3676f17eb21d3da996af021d7b83ed725
ytyaru <yttry0@gmail.com>
2024-07-30 18:52:04 +0900

    add 7

---
+++ docs/7/js/main.js
@@ -0,0 +1,151 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const {h1, p} = van.tags
+    const author = 'ytyaru'
+    van.add(document.querySelector('main'), 
+        h1(van.tags.a({href:`https://github.com/${author}/Html.VanJS.Valk.20240718164433/`}, 'Valk')),
+        p('値の代入をコールバック関数で制御するJavaScriptライブラリ。'),
+//        p('A JavaScript library that controls value assignment using callback functions.'),
+    )
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make())
+
+    const a = new Assertion()
+
+    // FixedValue
+    a.e(TypeError, 'Assignment to constant variable.', ()=>{
+        const zero = valk.fix(0)
+        zero = 1
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const zero = valk.fix(0)
+        zero.x = 1 // 存在しないプロパティ x に代入しようとした
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const zero = valk.fix(0)
+        zero.v = 1 // 存在するプロパティ v に代入しようとした
+    })
+    a.e(ReferenceError, `Property does not exist: x`, ()=>{
+        const zero = valk.fix(0)
+        return 0===zero.x // 存在しないプロパティ v を参照した
+    })
+    a.t(()=>{
+        const zero = valk.fix(0)
+        return 0===zero.v // 存在するプロパティ v を参照した
+    })
+    console.log(valk.fix(0).__type)
+    a.t('Proxy(valk.FixedValue)'===valk.fix(0).__type)
+
+    // FixedArray
+    a.t(Array.isArray(valk.fix([1,2,3])))
+    a.t(()=>{
+        const fixAry = valk.fix([1,2,3])
+        return 'Proxy(valk.FixedArray)'===fixAry.__type
+    })
+    a.t(()=>{
+        const fixAry = new valk.types.FixedArray([1,2,3])
+        return 'Proxy(valk.FixedArray)'===fixAry.__type
+    })
+    // static メソッドは継承しない？（Proxyによって阻まれる？）
+    a.e(TypeError, 'Cannot create proxy with a non-object as target or handler', ()=>{
+        const fixAry = valk.types.FixedArray.of([1,2,3]) // Array.of
+    })
+    a.e(TypeError, 'Cannot create proxy with a non-object as target or handler', ()=>{
+        const fixAry = valk.types.FixedArray.from([1,2,3]) // Array.from
+    })
+    a.e(TypeError, 'valk.types.FixedArray.fromAsync is not a function', async()=>{ // テスト環境で Array.fromAsync は未実装
+        const fixAry = await valk.types.FixedArray.fromAsync([1,2,3]) // Array.fromAsync
+    })
+    // なぜ isArray だけ継承される？
+    a.t(()=>{
+    //a.e(TypeError, 'Cannot create proxy with a non-object as target or handler', ()=>{
+        return valk.types.FixedArray.isArray([1,2,3]) // Array.from
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const fixAry = new valk.types.FixedArray([1,2,3])
+        fixAry[0] = 9
+    })
+    a.t(()=>{
+        const fixAry = valk.fix([1,2,3])
+        return 3===fixAry.length && 1===fixAry[0] && 2===fixAry[1] && 3===fixAry[2]
+    })
+    a.e(TypeError, 'Assignment to constant variable.', ()=>{
+        const fixAry = valk.fix([1,2,3])
+        console.log(fixAry)
+        fixAry = [4,5,6]
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const fixAry = valk.fix([1,2,3])
+        console.log(fixAry)
+        fixAry[0] = 9
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const fixAry = valk.fix([1,2,3])
+        console.log(fixAry)
+        fixAry.pop()
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const fixAry = valk.fix([1,2,3])
+        console.log(fixAry)
+        fixAry[0] = 9
+    })
+    for (let name of 'copyWithin,fill,pop,push,reverse,shift,sort,splice,unshift'.split(',')) {
+        a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+            const fixAry = valk.fix([1,2,3])
+            fixAry[name]()
+        })
+    }
+    a.t(()=>{
+        const fix = valk.fix([1])
+        const a = fix.concat([2])
+        console.log(a)
+        return 2===a.length && 1===a[0] && 2===a[1]
+    })
+    a.t(()=>{
+        const fix = valk.fix([1,2,3,4,5,6,7,8,9])
+        const a = fix.filter(v=>0===v%2)
+        return 4===a.length && [2,4,6,8].every((v,i)=>v===a[i])
+    })
+
+    // FixedSet
+    a.t(valk.fix(new Set([1,2,3])) instanceof Set)
+    a.e(ReferenceError, `Property does not exist: 0`, ()=>{
+        const s = valk.fix(new Set([1,2,3]))
+        s[0]
+    })
+    a.e(valk.errors.FixError, 'Assignment to fix variable.', ()=>{
+        const s = valk.fix(new Set([1,2,3]))
+        s[0] = 9
+    })
+    a.t(()=>{
+        const s = valk.fix(new Set([1,2,3]))
+        console.log(s.size)
+        return 3===s.size
+    })
+    a.t(()=>{
+        const s = valk.fix(new Set([1,2,3]))
+        console.assert(s instanceof Set)
+        return 3===s.size && [1,2,3].every(v=>s.has(v))
+    })
+    a.t(()=>{
+        const s = valk.fix(new Set([1,2,3]))
+        const a = [...s.values()]
+        return 3===a.length && 1===a[0] && 2===a[1] && 3===a[2]
+    })
+    a.t(()=>{
+        const r = []
+        const expected = [1,2,3]
+        const s = valk.fix(new Set(expected))
+        let i = 0
+        for (let v of s.values()) {
+            r.push(v===expected[i])
+            i++
+        }
+        return r.every(v=>v)
+    })
+
+    a.fin()
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
---
+++ docs/7/js/valk.js
@@ -0,0 +1,207 @@
+(function(){
+class FixError extends Error { constructor(msg=`Assignment to fix variable.`) { super(msg); this.name='FixError' } }
+class CounterError extends Error { constructor(msg=`Assignment to counter variable.`) { super(msg); this.name='CounterError' } }
+const valk = {
+    fix:(v)=>FixValue.get(v),
+    count:(v, options)=>{
+        if (!(Number(n) === n && n % 1 === 0)) { v = 0 }
+        if ('Object'!==options.constructor.name) { options = {step:(tick)=>tick++} }
+    },
+    errors: {
+        FixError: FixError,
+    },
+}
+class FixValue {
+    static get(v) {
+        if (Array.isArray(v)) { return new FixedArray(v) }
+        if (v instanceof Set) { return new FixedSet(v) }
+        if (v instanceof Map) { return new FixedMap(v) }
+        if ('Object'===v.constructor.name) { return new FixedMap(v) }
+        return this._getProxy({v:v})
+    }
+    static _getProxy(obj) {
+        return new Proxy(obj, {
+            set(target, key, value, receiver) { throw new FixError() },
+            get(target, key, receiver) {
+                if ('__type'===key) { return `Proxy(valk.FixedValue)` }
+                if (key in target) { return Reflect.get(target, key) }
+                else { throw new ReferenceError(`Property does not exist: ${key}`) }
+            },
+        })
+    }
+}
+const ChangenableContainer = superClass => class extends superClass {
+    constructor(...args) { super(...args); this._onChanged = ()=>{}; }
+    get onChanged() { return this._onChanged }
+    set onChanged(v) { if ('function'===typeof v) { this._onChanged = v } }
+    _setupOnChanged(names) {
+        for (let name of names) {
+            Object.defineProperty(this, name, {
+                value:(...args)=>{
+                    super[name](...args)
+                    this._onChanged(this)
+                },
+                writable: true,
+                enumerable: true,
+                configurable: true,
+            })
+        }
+    }
+}
+class ChangedArray extends ChangenableContainer(Array) { // Arrayの破壊的メソッドを監視する
+    constructor(...args) {
+        super(...args)
+        this._setupOnChanged('copyWithin,fill,pop,push,reverse,shift,sort,splice,unshift'.split(','))
+    }
+}
+class FixedArray extends ChangedArray {
+    constructor(...args) {
+        super(...args)
+        this._onChanged = ()=>{throw new FixError()}
+        return this._getProxy(...args)
+    }
+    _getProxy(...args) {
+        return new Proxy(...args, {
+            set(target, key, value, receiver) { throw new FixError() },
+            get(target, key, receiver) {
+                if ('__type'===key) { return `Proxy(valk.FixedArray)` }
+                return Reflect.get(target, key)
+//                if (key in target) { return Reflect.get(target, key) }
+//                else { throw new ReferenceError(`Property does not exist: ${key}`) }
+            },
+        })
+    }
+}
+class ChangedSet extends ChangenableContainer(Set) {
+    constructor(...args) {
+        super(...args)
+        this._setupOnChanged('add,clear,delete'.split(','))
+    }
+}
+class FixedSet extends ChangedSet {
+    constructor(...args) {
+        super(...args)
+        this._onChanged = ()=>{throw new FixError()}
+        return this._getProxy(...args)
+    }
+    _getProxy(...args) {
+        return new Proxy(...args, {
+            set(target, key, value, receiver) { throw new FixError() },
+            get(target, key, receiver) {
+                if ('__type'===key) { return `Proxy(valk.FixedSet)` }
+                if (key in target) {
+                    if (Type.hasGetter(target, key)) { return Reflect.get(target, key) }
+                    else if ('function'===typeof target[key]) { return target[key].bind(target) }
+                }
+                else { throw new ReferenceError(`Property does not exist: ${key}`) }
+            },
+            apply(target, thisArg, argumentsList) {
+                console.log(target, thisArg, argumentsList)
+                return target(...argumentsList)
+            },
+        })
+    }
+}
+class ChangedMap extends ChangenableContainer(Map) {
+    constructor(...args) {
+        super(...args)
+        this._setupOnChanged('clear,delete,set'.split(','))
+        //this._setupOnChanged('add,clear,delete,set'.split(','))
+    }
+}
+class FixedMap extends ChangedMap {
+    constructor(...args) {
+        if (1===args.length && 'Object'===args[0].constructor.name) { super([...Object.entries(args[0])]) }
+        else if (1===args.length && args[0] instanceof Map) { super([...args[0].entries()]) }
+        else { super(...args) }
+//        if (1===args.length && ('Object'===args[0].constructor.name || args[0] instanceof Map)) { console.log('*********', );super(Array.from(Object.entries(args[0]))) } else { super(...args) }
+//        (1===args.length && 'Object'===args[0].constructor.name) ? super(Array.from(Object.entries(args[0]))) : super(...args);
+        this._onChanged = ()=>{throw new FixError()};
+    }
+//    constructor(...args) { super(...args); this._onChanged = ()=>{throw new FixError()}; }
+//    static get [Symbol.species]() { return Array; }
+}
+
+/*
+class ChangedWeakSet extends ChangenableContainer(WeakSet) {
+    constructor(...args) {
+        super(...args)
+        this._setupOnChanged('add,delete'.split(','))
+    }
+}
+class FixedWeakSet extends ChangedWeakSet {
+    constructor(...args) { super(...args); this._onChanged = ()=>{throw new FixError()}; }
+//    static get [Symbol.species]() { return Array; }
+}
+class ChangedWeakMap extends ChangenableContainer(WeakMap) {
+    constructor(...args) {
+        super(...args)
+        this._setupOnChanged('add,delete,set'.split(','))
+    }
+}
+class FixedWeakMap extends ChangedWeakMap {
+    constructor(...args) { super(...args); this._onChanged = ()=>{throw new FixError()}; }
+//    static get [Symbol.species]() { return Array; }
+}
+*/
+
+
+/*
+class ChangedContainer { // 破壊的メソッドを監視する
+    constructor(...args) { this._onChanged = ()=>{}; this._setupOnChanged(); }
+    get onChanged() { return this._onChanged }
+    set onChanged(v) { if ('function'===typeof v) { this._onChanged = v } }
+    _setupOnChanged(names) {
+        Object.defineProperty(this, name, (...args)=>{
+            super[name](...args)
+            this._onChanged(this)
+        })
+    }
+
+}
+class ChangedArray extends Array { // 破壊的メソッドを監視する
+    constructor(...args) { super(...args); this._onChanged = ()=>{}; this._setupOnChanged(); }
+    get onChanged() { return this._onChanged }
+    set onChanged(v) { if ('function'===typeof v) { this._onChanged = v } }
+    _setupOnChanged() {
+        const names = 'copyWithin,fill,pop,push,reverse,shift,sort,splice,unshift'.split(',')
+        Object.defineProperty(this, name, (...args)=>{
+            super[name](...args)
+            this._onChanged(this)
+        })
+    }
+//    copyWithin(...args) { super.copyWithin(...args); this._onChanged(this); }
+//    fill(...args) { super.fill(...args); this._onChanged(this); }
+}
+*/
+class Counter {
+    constructor(v, options) {
+    //constructor(v, step) {
+        if (!(Number(n) === n && n % 1 === 0)) { v = 0 }
+        if ('Object'!==options.constructor.name) { this._options = {step:(v, tick, op, ...args)=>v++, threshold:null, onCompleted:(v,tick,op)=>{}} }
+        //if ('function'!==typeof step) { this._step = (tick)=>tick++; }
+        this._tick = 0
+        this.v = v
+    }
+    get v() { return this._v }
+    set v(v) { throw new CounterError }
+    count(...args) { this._v = this._options.step(this._v, this._tick, this._option, ...args); this._tick++; this._runCompleted(); return this._v; }
+    _runCompleted() {
+        if (Number.isNaN(this._options.threshold)) { return }
+        if (this._options.threshold <= this._v) {
+            if ('AsyncFunction'===this._options.onCompleted.constructor.name) { this._options.onCompleted(this._v, this._tick, this._option).then(()=>{}).catch(e=>{throw e}) }
+            else if ('function'===this._options.onCompleted) { this._options.onCompleted(this._v, this._tick) }
+        }
+    }
+}
+
+valk.types = {
+    FixedArray: FixedArray,
+    FixedSet: FixedSet,
+    FixedMap: FixedMap,
+//    FixedWeakSet: FixedWeakSet,
+//    FixedWeakMap: FixedWeakMap,
+}
+window.valk = valk
+//window.FixError = FixError
+})()
